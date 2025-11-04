4d57520db1dbeb5ad610ce180998b36a6a1241d6
Data-solutionsweb <pravardhan@data-solutions.org>
2025-11-04 17:15:06 +0530

    oo

--- src/components/SidebarTag.jsx
+++ src/components/SidebarTag.jsx
@@ -6,7 +6,7 @@ export default function SidebarTag() {
     <div className="fixed top-1/2 right-[-35px] transform -translate-y-1/2 rotate-180 z-50">
       <Link href="/IJeeHero">
         <div className="rotate-90 bg-gradient-to-b from-green-400 to-teal-600 text-white px-4 py-2 rounded-t-lg shadow-lg font-semibold cursor-pointer hover:opacity-90 transition">
-          IRST JEE – 2025
+          IRST JEE – 2026
         </div>
       </Link>
     </div>
